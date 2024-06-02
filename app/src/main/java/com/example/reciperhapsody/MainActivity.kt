b39e41f4505e259294072e1e63ff708bbaab8544
kishibee <raflyade9@gmail.com>
2024-06-02 22:29:33 +0700

    setting navigation

--- app/src/main/java/com/example/reciperhapsody/MainActivity.kt
+++ app/src/main/java/com/example/reciperhapsody/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.reciperhapsody.ui.theme.RecipeRhapsodyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeRhapsodyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "home_screen"
+                ) {
+                    composable("home_screen") {
+
+                    }
+                    composable("detail_screen") {
+
+                    }
+                    composable("create_screen") {
+
+                    }
+                    composable("favorite_screen") {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeRhapsodyTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/reciperhapsody/RecipeRhapsodyApplicaton.kt
@@ -0,0 +1,4 @@
+package com.example.reciperhapsody
+
+class RecipeRhapsodyApplicaton {
+}
