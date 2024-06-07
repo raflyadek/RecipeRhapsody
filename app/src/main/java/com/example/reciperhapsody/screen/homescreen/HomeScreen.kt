b9da73db68642e3e38ac60e3aa46acf0886fd6fe
kishibee <raflyade9@gmail.com>
2024-06-07 22:30:23 +0700

    homescreen and searchbar

--- app/src/main/java/com/example/reciperhapsody/screen/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/reciperhapsody/screen/homescreen/HomeScreen.kt
@@ -1,2 +1,63 @@
 package com.example.reciperhapsody.screen.homescreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun RecipeHomeScreen(
+    navController: NavController,
+    viewModel: HomeScreenViewModel = hiltViewModel()
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(25.dp))
+            Image(
+                painter = ,
+                contentDescription = "Tagline text",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+    val focusManager = LocalFocusManager.current
+
+    Box(modifier = modifier) {
+        //using what text field?
+    }
+}
