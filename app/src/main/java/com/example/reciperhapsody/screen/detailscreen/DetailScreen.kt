76665b68b448e85ec7ccc77206925dc502b7d437
kishibee <raflyade9@gmail.com>
2024-06-10 20:54:14 +0700

    RecipeListEntry and RecipeRow

--- app/src/main/java/com/example/reciperhapsody/screen/detailscreen/DetailScreen.kt
+++ app/src/main/java/com/example/reciperhapsody/screen/detailscreen/DetailScreen.kt
@@ -1,2 +1,5 @@
 package com.example.reciperhapsody.screen.detailscreen
 
+
+
+//Use SHARED CONTENT
--- app/src/main/java/com/example/reciperhapsody/screen/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/reciperhapsody/screen/homescreen/HomeScreen.kt
@@ -3,17 +3,22 @@ package com.example.reciperhapsody.screen.homescreen
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.foundation.text2.input.TextFieldLineLimits
 import androidx.compose.foundation.verticalScroll
@@ -23,6 +28,7 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -38,19 +44,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
 import com.example.reciperhapsody.R
+import com.example.reciperhapsody.data.RecipeListEntry
 import com.example.reciperhapsody.data.model.BottomBarItem
+import com.example.reciperhapsody.data.remote.response.RecipeList
 
 @Composable
 fun RecipeHomeScreen(
@@ -162,6 +176,81 @@ fun BottomBar(modifier: Modifier = Modifier) {
                 }
             )
         }
+    }
+}
+
+@Composable
+fun RecipeEntry(
+    entry: RecipeListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+//    viewModel: HomeScreenViewModel = hiltViewModel()
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .clickable {
+                navController.navigate(
+                    "detail_screen"
+                )
+            }
+    ) {
+        Column {
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(entry.imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = entry.recipeName,
+                loading = {
+                    CircularProgressIndicator()
+                },
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = entry.recipeName,
+                fontSize = 20.sp,
+//              fontFamily = font apa?
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun RecipeRow(
+    rowIndex: Int,
+    entries: List<RecipeListEntry>,
+    navController: NavController
+) {
+    Column {
+        Row {
+            //first entry
+            RecipeEntry(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            //Check if there is a second entry in this row
+            if (entries.size >= rowIndex * 2 + 1) {
+                //second entry
+                RecipeEntry(
+                    entry = entries[rowIndex * 2 +1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
